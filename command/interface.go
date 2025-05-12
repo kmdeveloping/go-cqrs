a3334356733311092c6e1bd5eb2e6dca76f164d6
kmollencopf <km@kmdeveloping.com>
2025-05-12 09:21:05 -0400

    qwip

--- command/interface.go
+++ command/interface.go
@@ -4,7 +4,7 @@ type ICommand any
 
 // ICommandHandler interface handles commands of type T that implements ICommand
 // The T can be either a value or a pointer type that implements ICommand
-type ICommandHandler[T any] interface {
+type ICommandHandler[T ICommand] interface {
 	Handle(T) error
 }
 
